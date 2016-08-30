2844bf2899a29c9ff58cfb53909abcdc3e31c1e3
Gabriel Roldan <luisgabrielroldan@gmail.com>
2016-08-29 21:50:06 -0300

    Added helper to read configuration from artik.properties file.

---
+++ src/CLOUD.Artik.Test/Api/ArtikCloudApiTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using CLOUD.Artik.Test.Utils;
+
+namespace CLOUD.Artik.Test
+{
+    public abstract class ArtikCloudApiTest
+    {
+        private const string PROPERTIES_FILENAME = "artik.properties";
+        protected Properties Properties = new Properties();
+
+        /**
+         * Loads the artik.properties
+         */
+
+        protected ArtikCloudApiTest()
+        {
+            Properties.LoadFromFile(PROPERTIES_FILENAME);
+        }
+    }
+}
---
+++ src/CLOUD.Artik.Test/Utils/Properties.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CLOUD.Artik.Test.Utils
+{
+    /// <summary>
+    ///     Reads a property list (key and element pairs) from the input
+    ///     character stream in a simple line-oriented format.
+    /// </summary>
+    public class Properties : Dictionary<string, string>
+    {
+        public void LoadFromFile(string fileName)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                LoadFromStream(fs);
+            }
+        }
+
+        public void LoadFromStream(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    ParseLine(this, line);
+                }
+            }
+        }
+
+        private void ParseLine(IDictionary<string, string> properties, string line)
+        {
+            if ((String.IsNullOrEmpty(line)) ||
+                (line.StartsWith(";")) ||
+                (line.StartsWith("#")) ||
+                (line.StartsWith("'")) ||
+                (!line.Contains('=')))
+                return;
+
+            var index = line.IndexOf('=');
+            var key = line.Substring(0, index).Trim();
+            var value = line.Substring(index + 1).Trim();
+
+            if ((value.StartsWith("\"") && value.EndsWith("\"")) ||
+                (value.StartsWith("'") && value.EndsWith("'")))
+            {
+                value = value.Substring(1, value.Length - 2);
+            }
+
+            // Ignore duplicates
+            if (!properties.ContainsKey(key))
+                properties.Add(key, value);
+        }
+    }
+}
